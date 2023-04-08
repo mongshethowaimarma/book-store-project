6e20c81b681c05f9ac497b2ba78e96c709b78183
mongshethoiwmarma1 <mongshethowaimarma.pstu@gmail.com>
2023-04-08 15:32:07 +0600

    added single book

--- src/components/Book.jsx
+++ src/components/Book.jsx
@@ -3,12 +3,12 @@ import React from 'react';
 const Book = ({book}) => {
     
     return (
-        <div className='overflow-hidden relative transition duration-200 trnsform hover:-translate-y-2 rounded shadow-lg hover:shadow-2xl'>
+        <div className='overflow-hidden relative transition duration-200 transform hover:-translate-y-2 rounded shadow-lg hover:shadow-2xl'>
             <img src={book.image} alt="book cover" className='object-cover w-full h-56 md:h-64 xl:h-80'/>
-            <div>
+            <div className='bg-black px-6 py-4 bg-opacity-75 opacity-0 hover:opacity-100 text-gray-300 absolute inset-0 transition-opacity duration-200 flex flex-col'>
                 <p>{book.title}</p>
                 <p>{book.subtitle.substring(0, 45)}.....</p>
-                <p>Price: {book.price}</p>
+                <p className='mt-auto'>Price: {book.price}</p>
             </div>
         </div>
     );
