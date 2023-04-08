5ee2a6142c7d89832b9c9fbfb1907647db158ff0
mongshethoiwmarma1 <mongshethowaimarma.pstu@gmail.com>
2023-04-08 16:20:34 +0600

    added dynamic id

--- src/main.jsx
+++ src/main.jsx
@@ -9,6 +9,7 @@ import {
 import Home from './components/Home';
 import About from './components/About';
 import Books from './components/Books';
+import BookDetails from './components/BookDetails';
 
 const router = createBrowserRouter([
   {
@@ -23,6 +24,11 @@ const router = createBrowserRouter([
         path: 'books',
         element:<Books></Books>,
         loader: ()=> fetch('https://api.itbook.store/1.0/new'),
+      },
+      {
+        path: 'book/:id',
+        element: <BookDetails></BookDetails>,
+        loader: ({params})=> fetch(`https://api.itbook.store/1.0/books/${params.id}`)
       },
       {
         path: 'about',
